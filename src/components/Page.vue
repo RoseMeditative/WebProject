a9237904398bcdf71d702c29852e65616e3c2556
RoseMeditative <abiau.pro@outlook.fr>
2019-10-03 18:46:00 +0200

    Fixing bugs and trying to test differents functions

---
+++ src/components/Page.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-container>
+        <v-layout text-center wrap>
+
+            <v-form v-model="valid">
+
+                <v-container v-if="notConnect">
+                    
+                    <v-btn rounded color="orange" dark>Rounded Button</v-btn>
+
+                    <v-container v-if="notRegister">
+
+                    <v-container>
+                    <v-container v-if="Register">
+                    <v-container>
+
+
+
+
+                </v-container>
+
+
+    
+
+            </v-form>
+
+        </v-layout>
+    </v-container>
+    
+</template>
